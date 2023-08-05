94cb3a61dd6b6008dcad3016a1e3fae87f87cffd
sehsekou98 <sehsekou98@gmail.com>
2023-08-05 23:22:25 +0100

    filter

--- src/customer/components/Product/productCard.jsx
+++ src/customer/components/Product/productCard.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import "./ProductCard.css"
+import React from "react";
+import "./ProductCard.css";
 
-const productCard = () => {
+const productCard = ({product}) => {
   return (
     <div>
-       <div className='productCard w-[15rem] m-3 transition-all
-    cursor-pointer'>
-        <div className='h-[20rem]'>
-            <img className='h-full w-full object-cover object-left-top'
-             src="https://ae01.alicdn.com/kf/S2605a9d9948b461c9c95117741b520a7q/Silky-French-Curl-Braiding-Hair-22-Inch-Loose-Wave-Spanish-Curly-Crochet-Braids-Pre-Stretched-Bouncy.jpg_220x220xz.jpg_.webp" alt="" />
+      <div
+        className="productCard w-[15rem] m-3 transition-all
+    cursor-pointer"
+      >
+        <div className="h-[20rem]">
+          <img
+            className="h-full w-full object-cover object-left-top"
+            src={product.imageUrl}
+          />
         </div>
-        <div className='textPart bg-white p-3'>
+        <div className="textPart bg-white p-3">
           <div>
-            <p className='font-bold opacity-60'>Stretched Length : 24 inchess</p>
-            <p>Kalon Ombre Braiding Hair Synthetic Braiding Hair</p>
+            <p className="font-bold opacity-60">
+              {product.brand}
+            </p>
+            <p>{product.title}</p>
           </div>
-          <div className='flex items-center space-x-2'>
-            <p className='font-semibold'>$200</p>
-            <p className='line-through opacity-50'>$2000</p>
-            <p className='text-green-600 font-semibold'>50% off</p>
+          <div className="flex items-center space-x-2">
+            <p className="font-semibold">{product.discountedPrice}</p>
+            <p className="line-through opacity-50">{product.price}</p>
+            <p className="text-green-600 font-semibold">{product.discountPresent}off</p>
           </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default productCard
+export default productCard;
